f5022c8b8a8ca6b518a0a460ad236a7b5b9fa8cf
Yuriy Shevtsov <hirurjek@gmail.com>
2025-07-28 18:24:58 +0100

    HW3

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -14,3 +14,30 @@
   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
   */
+
+  let minAge = 18;
+  let maxAge = 60;
+
+  function checkAge(age) {
+    if (typeof age !== 'number') {
+      console.log("Incorrect data type");
+  } else {
+      if (age < minAge) {
+          console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+      } else if (age >= minAge && age < maxAge) {
+          console.log("Welcome  !");
+      } else if (age > maxAge) {
+          console.log("Keep calm and look Culture channel.");
+      } else {
+          console.log("Technical work.");
+      }
+  }}
+
+
+checkAge(10);
+checkAge(17);
+checkAge(18);
+checkAge(19);
+checkAge(59);
+checkAge(60);
+checkAge(61);
--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,34 @@
   преобразовываясь в number
 
 */
+
+
+  let minAge = 18;
+  let maxAge = 60;
+  
+  function checkAge(age) {
+    age = Number(age);
+    console.log(age, typeof{age}); 
+
+    if (typeof age !== 'number') {
+      console.log("Incorrect data type");
+  } else {
+      if (age < minAge) {
+          console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+      } else if (age >= minAge && age < maxAge) {
+          console.log("Welcome  !");
+      } else if (age > maxAge) {
+          console.log("Keep calm and look Culture channel.");
+      } else {
+          console.log("Technical work.");
+      }
+  }}
+
+
+checkAge('фис');
+checkAge(17);
+checkAge(18);
+checkAge(19);
+checkAge(59);
+checkAge(60);
+checkAge(61);
